9c779f3561a93863aaf8be3ca3662668771de4a1
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-10-23 07:02:12 +0100

    feat(publish): implement semantic-release publish stage

---
+++ index.ts
@@ -0,0 +1,105 @@
+import {concatMapFn, copy, mapFn} from "@softwareventures/array";
+import chain from "@softwareventures/chain";
+import {Credentials, S3} from "aws-sdk";
+import * as fs from "fs";
+import globby = require("globby");
+import * as os from "os";
+import * as path from "path";
+import {Context} from "semantic-release";
+
+export interface Config {
+    readonly uploads: ReadonlyArray<Upload>;
+}
+
+export interface Upload {
+    readonly src: {
+        readonly dir: string;
+        readonly include?: ReadonlyArray<string>;
+        readonly exclude?: ReadonlyArray<string>;
+    };
+    readonly dest: {
+        readonly bucket: string;
+        readonly prefix?: string;
+    };
+}
+
+export async function publish(config: Config, context: Context): Promise<void> {
+    const {logger} = context;
+
+    const accessKeyId = context.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = context.env.AWS_SECRET_ACCESS_KEY;
+
+    const s3 = new S3({
+        apiVersion: "2006-03-01",
+        credentials: new Credentials(accessKeyId, secretAccessKey),
+        sslEnabled: true,
+        computeChecksums: true
+    });
+
+    return chain(config.uploads)
+        .map(mapFn(({src, dest}) => ({
+            src: {
+                dir: src.dir,
+                files: globby(src.include || "**", {
+                    cwd: src.dir,
+                    ignore: copy(src.exclude || []),
+                    onlyFiles: true
+                })
+            },
+            dest
+        })))
+        .map(mapFn(({src, dest}) => src.files
+            .then(mapFn(file => ({
+                file: path.resolve(src.dir, file),
+                dest: {
+                    bucket: dest.bucket,
+                    key: dest.prefix
+                        ? dest.prefix + "/" + file
+                        : file
+                }
+            })))))
+        .map(files => Promise.all(files))
+        .map(files => files.then(concatMapFn(file => file)))
+        .map(files => files
+            .then(mapFn(({file, dest}) => () => {
+                logger.log(`Uploading ${file} to ${dest.bucket}:${dest.key}`);
+                return new Promise<S3.PutObjectOutput>((resolve, reject) =>
+                    s3.putObject({
+                        Bucket: dest.bucket,
+                        Key: dest.key,
+                        Body: fs.createReadStream(file)
+                    }, (err, data) => err ? reject(err) : resolve(data)));
+            })))
+        .map(actions => actions.then(parallel))
+        .map(result => result.then((_: ReadonlyArray<S3.PutObjectOutput>) => void 0))
+        .value;
+}
+
+function parallel<T>(tasks: ReadonlyArray<() => Promise<T>>, threads = 0): Promise<T[]> {
+    if (threads <= 0) {
+        threads = os.cpus().length * 2;
+    }
+
+    const results: Array<Promise<T>> = new Array(tasks.length);
+    let nextTaskIndex = 0;
+
+    function run(): Promise<void> {
+        const i = nextTaskIndex++;
+        if (i < tasks.length) {
+            const result = tasks[i]();
+            results[i] = result;
+
+            return result.then(run);
+        } else {
+            return Promise.resolve();
+        }
+    }
+
+    const runs: Array<Promise<void>> = [];
+    for (let i = 0; i < threads; ++i) {
+        runs.push(run());
+    }
+
+    return Promise.all(runs)
+        .then(() => Promise.all(results));
+}
